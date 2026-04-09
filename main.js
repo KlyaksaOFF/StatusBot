c8a3b39a4ff99dd70824b1fc14de080823f98360
Alex <klyaksaoff@gmail.com>
2026-04-09 16:38:53 +0300

    updated

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 import 'dotenv/config'
 import { Telegraf } from 'telegraf';
-import{ message } from 'telegraf/filters';
-
+import { message } from 'telegraf/filters';
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-
 const requestUrl = async (url) => {
     try {
         const response = await fetch(url)
@@ -17,7 +15,21 @@ const requestUrl = async (url) => {
         }
 }
 
-// const result = await requestUrl('')
+bot.command('status', async (ctx) => {
+    const args = ctx.payload;
+    if (!args) {
+        return ctx.reply('Specify the url after the command!');
+    }
+    try {
+
+        const resultCheckUrl = await requestUrl(args)
+        await ctx.reply(resultCheckUrl);
+    } catch(error) {
+        await ctx.reply('Error')
+    }
+
+})
+
 
 
 
