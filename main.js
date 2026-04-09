78e5e996a1728987e047f04e856b820df6c1c518
Alex <klyaksaoff@gmail.com>
2026-04-09 15:53:38 +0300

    update

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const { Telegraf } = require('telegraf')
+const { message } = require('telegraf/filters')
+
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
 const requestUrl = async (url) => {
     try {
         const response = await fetch(url)
